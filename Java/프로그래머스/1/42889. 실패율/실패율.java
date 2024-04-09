ccfc39d70cc69d420ec1bf21dbde010412c0b036
enqnWkd <127079161+enqnWkd@users.noreply.github.com>
2024-04-09 16:25:02 +0900

    [level 1] Title: 실패율, Time: 1.57 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int N, int[] stages) {
+        Map<Integer, Double> map = new HashMap<>();
+        int fail, less=0;
+        
+        Arrays.sort(stages);
+        for (int i=1; i<=N; i++) {
+            fail = 0;
+            for (int j = less; j<stages.length; j++) {
+                if (stages[j] < i) { less++; continue; }
+                else if (stages[j] == i) fail++;
+                else break;
+            }
+            if (stages.length-less == 0) map.put(i, 0.0);
+            else map.put(i, (double)fail / (stages.length - less));
+        }
+        
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+        Collections.sort(keySet, (v1, v2) -> (map.get(v2).compareTo(map.get(v1))));
+        
+        for (int k : keySet)
+            System.out.println(map.get(k));
+        return keySet;
+    }
+    
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         int n = 5;
+//         int[] stages = {1,1,1,2,3,4};
+//         sol.solution(n, stages);
+//     }
+}
