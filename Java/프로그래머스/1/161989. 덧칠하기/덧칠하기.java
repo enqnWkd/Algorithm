68d807b55f68b1b175cb1c27be9dfe198623149f
enqnWkd <127079161+enqnWkd@users.noreply.github.com>
2024-08-20 01:22:29 +0900

    [level 1] Title: 덧칠하기, Time: 2.25 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 1;
+        
+        int temp = section[0];
+        for (int i=1; i<section.length; i++) {
+            if (section[i]-temp+1 > m) {
+                answer++;
+                temp = section[i];
+            }
+        }
+        return answer;
+    }
+}
