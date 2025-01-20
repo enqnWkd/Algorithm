365de7fab0f4b4c83f25a44f8c52b27eb965bd86
enqnWkd <127079161+enqnWkd@users.noreply.github.com>
2025-01-21 04:35:24 +0900

    [level 1] Title: K번째수, Time: 1.22 ms, Memory: 76 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,22 +4,18 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         
         int[] answer = new int[commands.length];
-        List<Integer> list = new ArrayList<>();
         
         for (int i=0; i<commands.length; i++) {
             
-            for(int j=0; j<commands[i][1]-commands[i][0]+1; j++) {
-                list.add(array[j+commands[i][0]-1]);
-            }
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int k = commands[i][2] - 1;
             
-            Collections.sort(list, new Comparator<Integer>() {
-                @Override
-                public int compare(Integer i1, Integer i2) {
-                    return i1 - i2;
-                }
-            });
-            answer[i] = list.get(commands[i][2]-1);
-            list.clear();
+            int[] subArray = Arrays.copyOfRange(array, start, end);
+            
+            Arrays.sort(subArray);
+            
+            answer[i] = subArray[k];
         }
         return answer;
     }
